aa7b2ebb0c7f5f2f40a379bd39e3759cfb7e5aac
Bryan <Bryan@DESKTOP-Q2BO73H>
2018-10-17 12:34:41 -0400

    Added sum class

---
+++ src/edu/temple/cis/c3238/banksim/Sum.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.temple.cis.c3238.banksim;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Sum extends Thread{
+    
+    private final Bank bank;
+    
+    public Sum(Bank b){
+        bank = b;
+    }
+    
+    @Override
+    public void run(){
+        try{
+            bank.test();
+        }
+        catch(InterruptedException exception){
+            
+        }
+    }
+    
+}
